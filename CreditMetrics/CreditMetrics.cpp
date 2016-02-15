b14e47f5de71d47a34fee51556c982c0270f924a
mbarnhill <mariebarnhill@gmail.com>
2016-02-15 11:11:54 -0500

    Added Parsing of Portfolio Data

--- CreditMetrics/CreditMetrics.cpp
+++ CreditMetrics/CreditMetrics.cpp
@@ -6,7 +6,19 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <algorithm>
+#include <functional> 
+#include <cctype>
+#include <locale>
 using namespace std;
+
+static inline string& trim(string& s) {
+	//left
+	s.erase(s.begin(), find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace))));
+	//right
+	s.erase(find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(), s.end());
+	return s;
+}
 
 template <class R> class CSV
 {
@@ -55,10 +67,10 @@ template <class R> class CSV
 	private:
 		vector<R> rows;
 };
-class Issuer
+class IssuerRating
 {
 	public: 
-		Issuer(vector<string>& cells):
+		IssuerRating(vector<string>& cells):
 			name(cells.at(0)),
 			rating(cells.at(1)),
 			industry(cells.at(2))
@@ -67,16 +79,85 @@ class Issuer
 		const string rating;
 		const string industry;
 };
-class Issuers : public CSV<Issuer>
+class Issuers : public CSV<IssuerRating>
 {
 	public:
 		Issuers() : CSV("issuers.csv", true) { }
-		Issuer* getByName(string name)
+		IssuerRating* getByName(string name)
 		{
 			for (size_t i = 0, n = size(); i < n; i++)
 			{
-				Issuer& issuer = get(i);
+				IssuerRating& issuer = get(i);
 				if(issuer.name == name)
+					return &issuer;
+			}
+			return nullptr;
+		}
+};
+class PortfolioEntry
+{
+	public:
+		PortfolioEntry(vector<string>& cells) :
+			name(cells.at(0)),
+			instrumentType(cells.at(1)),
+			cusip(cells.at(2)),
+			notional(convertNotional(cells.at(3))),
+			maturity(cells.at(4)),
+			coupon(convertPercent(cells.at(5))),
+			couponsPerYear(convertInt(cells.at(6))),
+			price(convertPrice(cells.at(7))),
+			yield(convertPercent(cells.at(8))),
+			cleanPrice(convertPrice(cells.at(9))),
+			exprr(convertPercent(cells.at(10)))
+		{}
+		const string name;
+		const string instrumentType;
+		const string cusip;
+		const int notional;
+		const string maturity;
+		const double coupon;
+		const int couponsPerYear;
+		const double price;
+		const double yield;
+		const double cleanPrice;
+		const double exprr;
+	private:
+		int convertInt(string strInt)
+		{
+			trim(strInt);
+			return ::atoi(strInt.c_str());
+		}
+		double convertDouble(string strDouble)
+		{
+			trim(strDouble);
+			return ::atof(strDouble.c_str());
+		}
+		int convertNotional(string strNotional)
+		{
+			return convertInt(strNotional) * 1000000;
+		}
+		double convertPercent(string strPercent)
+		{
+			replace(strPercent.begin(), strPercent.end(), '%', ' ');
+			return convertDouble(strPercent) / 100;
+		}
+		double convertPrice(string strPrice)
+		{
+			replace(strPrice.begin(), strPrice.end(), '$', ' ');
+			return convertDouble(strPrice);
+		}
+		
+};
+class PortfolioData : public CSV<PortfolioEntry>
+{
+	public:
+		PortfolioData() : CSV("portfolio_for_project.csv", true) { }
+		PortfolioEntry* getByName(string name)
+		{
+			for (size_t i = 0, n = size(); i < n; i++)
+			{
+				PortfolioEntry& issuer = get(i);
+				if (issuer.name == name)
 					return &issuer;
 			}
 			return nullptr;
@@ -89,8 +170,14 @@ int main(int argc, char* argv[])
 		Issuers issuers;
 		for (size_t i = 0, n = issuers.size(); i < n; i++)
 		{
-			Issuer issuer = issuers.get(i);
+			IssuerRating issuer = issuers.get(i);
 			cout << issuer.name << "," << issuer.rating << "," << issuer.industry << "\n";
+		}
+		PortfolioData portfolioData;
+		for (size_t i = 0, n = portfolioData.size(); i < n; i++)
+		{
+			PortfolioEntry entry = portfolioData.get(i);
+			cout << entry.exprr << "," << entry.cleanPrice << "\n";
 		}
 	}
 	catch (const exception &e)
